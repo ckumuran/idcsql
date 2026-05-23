6cfa0c95c107b5f779e90cafd7b9c1951c50b35d
kumaran <k31311558@gmail.com>
2026-05-23 11:31:36 +0530

    Create TypingCursor.tsx

---
+++ apps/web/src/components/TypingCursor.tsx
@@ -0,0 +1,16 @@
+export default function TypingCursor() {
+
+    return (
+
+        <span
+            className="
+                inline-block
+                w-[10px]
+                h-[28px]
+                bg-red-500
+                ml-2
+                animate-pulse
+            "
+        />
+    );
+}
